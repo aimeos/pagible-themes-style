2df2d101f071acf5acec1cc560241a788c7b8aa9
aimeos <aimeos@aimeos.org>
2026-07-16 08:10:09 +0000

    Separated admin editor includes from theme (#262)

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -216,29 +216,6 @@
         </footer>
 
 
-        <link href="{{ cmstheme($page, 'pico.modal.min.css') }}" rel="preload" as="style">
-        <link href="{{ cmstheme($page, 'cms-lazy.css') }}" rel="preload" as="style">
-        <script defer src="{{ cmstheme($page, 'csrf.js') }}"></script>
-        <script defer src="{{ cmstheme($page, 'cms.js') }}"></script>
-        @stack('foot')
-
-        @foreach($page->ancestorsAndSelf as $navItem)
-            @if($text = cms($navItem, 'config.styles.data.text'))
-                <style>{!! $text !!}</style>
-            @endif
-        @endforeach
-
-        @foreach($page->ancestorsAndSelf as $navItem)
-            @if($text = cms($navItem, 'config.javascript.data.text'))
-                <script>{!! $text !!}</script>
-            @endif
-        @endforeach
-
-        @if(\Aimeos\Cms\Permission::can('page:save', auth()->user()))
-            <link href="{{ cmsasset('vendor/cms/admin/editor.css') }}" rel="preload" as="style">
-            <script defer src="{{ cmsasset('vendor/cms/admin/editor.js') }}"></script>
-        @else
-            <script defer src="{{ cmstheme($page, 'stats.js') }}"></script>
-        @endif
+        @include('cms::layouts.foot')
     </body>
 </html>
