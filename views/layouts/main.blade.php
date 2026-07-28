32659ccf1dabc6158adf75019344817e0e75b4bd
aimeos <aimeos@aimeos.org>
2026-07-28 09:35:24 +0000

    Added login to top navigation in all themes if available

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -167,6 +167,15 @@
                             @endif
                         </li>
                     @endforeach
+                    @if(Route::has('login'))
+                        <li class="login">
+                            <a href="{{ route('login') }}" title="{{ __('Login') }}" aria-label="{{ __('Login') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 4-4 4 3 4 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>
+                            </a>
+                        </li>
+                    @endif
                 </ul>
                 <ul class="menu-open show">
                     <li>
