2fbaca2521893815e1226f9100e3620407bd9b11
aimeos <aimeos@aimeos.org>
2026-08-07 10:21:00 +0000

    Use 0.12 versions

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -170,8 +170,8 @@
                     @if(Route::has('login'))
                         <li class="login">
                             <a href="{{ route('login') }}" title="{{ __('Login') }}" aria-label="{{ __('Login') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                                    <path d="M3 14s-1 0-1-1 1-4 4-4 4 3 4 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
                                 </svg>
                             </a>
                         </li>
